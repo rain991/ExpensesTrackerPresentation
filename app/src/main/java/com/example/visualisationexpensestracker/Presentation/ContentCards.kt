b2e844cd362c8ed5b6c6e2793fd3a478e82316fe
Savenko Ivan PC <savenkoivan931@gmail.com>
2023-11-25 22:11:10 +0100

    First commit

---
+++ app/src/main/java/com/example/visualisationexpensestracker/Presentation/ContentCards.kt
@@ -0,0 +1,19 @@
+package com.example.visualisationexpensestracker.Presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ContentCardType1(){
+
+}
+
+
+@Composable
+fun ContentCardType2(){
+
+}
+
+@Composable
+fun ContentCardType3(){
+
+}
---
+++ app/src/main/java/com/example/visualisationexpensestracker/Presentation/MainScreenComposable.kt
@@ -0,0 +1,15 @@
+package com.example.visualisationexpensestracker.ui.theme
+
+import android.net.http.HeaderBlock
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreenComposable() {
+    Column(modifier = 
+    Modifier.fillMaxWidth()){
+
+    }
+}
