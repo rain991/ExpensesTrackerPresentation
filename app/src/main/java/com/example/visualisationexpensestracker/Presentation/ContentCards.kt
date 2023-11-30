b1f195527066d272a82bffebf7f5183c7ea08e64
Savenko Ivan PC <savenkoivan931@gmail.com>
2023-11-30 16:56:55 +0100

    ModalBottomSheet created

--- app/src/main/java/com/example/visualisationexpensestracker/Presentation/ContentCards.kt
+++ app/src/main/java/com/example/visualisationexpensestracker/Presentation/ContentCards.kt
@@ -1,8 +1,10 @@
 package com.example.visualisationexpensestracker.Presentation
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,13 +12,23 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.visualisationexpensestracker.R
 
@@ -38,6 +50,8 @@ fun ExtendedButtonExample(onClick: () -> Unit, isExpanded: Boolean) {
             text = { Text(text = "Extended FAB") })
     }
 }}
+
+
 @Composable
 fun ExpensesCardTypeSimple() {
     Card(
@@ -46,5 +60,27 @@ fun ExpensesCardTypeSimple() {
             .padding(8.dp), shape = RoundedCornerShape(8.dp)
     ) {
         Text(text = stringResource(R.string.LoremIpsum))
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomBottomSheet(
+    onButtonClick: (String) -> Unit,
+    onDotClick: () -> Unit,
+    onOkayClick: () -> Unit,
+) {
+    val buttonValues = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+    val sheetState = rememberModalBottomSheetState()
+    var isSheetOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+    ModalBottomSheet(onDismissRequest = { Log.d("MyLog", "ModalBottomSheet dissmiss") }, sheetState = sheetState) {
+        Text(
+            text = "sdadasdasdasd",
+            color = androidx.compose.ui.graphics.Color.Yellow,
+        fontWeight = FontWeight.Bold
+        )
     }
 }
