02231aa36d52eacf79621251fa3eab7984624097
Savenko Ivan PC <savenkoivan931@gmail.com>
2023-11-30 21:15:52 +0100

    changes to expense adding(alignment)

--- app/src/main/java/com/example/visualisationexpensestracker/Presentation/ContentCards.kt
+++ app/src/main/java/com/example/visualisationexpensestracker/Presentation/ContentCards.kt
@@ -1,16 +1,20 @@
 package com.example.visualisationexpensestracker.Presentation
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,9 +34,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.visualisationexpensestracker.R
@@ -67,28 +73,87 @@ fun ExtendedButtonExample(isExpanded: Boolean) {
             Column(modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp)) {
-                Row {
+                Row(){
                     IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
-                        
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
                     }
                     Text(text = currentExpenseAdded.toString(), textAlign = TextAlign.Center, fontSize = 26.sp)
                 }
-                Row() {
-
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 6.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 6.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    VerticalAlignmentLine()
                 }
                 Row() {
-
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
                 }
                 Row() {
-
-                }
-                Row() {
-
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {  // Change Buttons Order Button
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+                    }
                 }
             }
         }
     }
 }
+
+@Composable
+fun VerticalAlignmentLine() {
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.width(80.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Okay",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
+
+
 //Text(
 //text = "sdadasdasdasd",
 //color = androidx.compose.ui.graphics.Color.Yellow,
